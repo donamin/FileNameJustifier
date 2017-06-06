fd4c1d2a8fbaec9620974abab485ada14bf4c80f
Amin Babadi <amin.babadi@aalto.fi>
2017-06-06 09:22:35 +0300

    first commit

---
+++ FileNameJustifier/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace FileNameJustifier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFolderAddress = new System.Windows.Forms.TextBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 15);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFolderAddress
+            // 
+            this.txtFolderAddress.Location = new System.Drawing.Point(93, 15);
+            this.txtFolderAddress.Name = "txtFolderAddress";
+            this.txtFolderAddress.Size = new System.Drawing.Size(303, 20);
+            this.txtFolderAddress.TabIndex = 1;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(402, 15);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 2;
+            this.btnRename.Text = "Rename Files";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 44);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.ScrollAlwaysVisible = true;
+            this.lstLog.Size = new System.Drawing.Size(465, 368);
+            this.lstLog.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 418);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.txtFolderAddress);
+            this.Controls.Add(this.btnBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFolderAddress;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.ListBox lstLog;
+    }
+}
+
---
+++ FileNameJustifier/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace FileNameJustifier
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderAddress.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(RenameAllFiles);
+            thread.Start();
+        }
+
+        void RenameAllFiles()
+        {
+            string[] listFolders = Directory.GetDirectories(txtFolderAddress.Text);
+            Log("Going through folders...");
+            for (int i = 0; i < listFolders.Length; i++)
+            {
+                string folderPath = listFolders[i];
+                string folderName = folderPath.Substring(folderPath.LastIndexOf('\\') + 1);
+                Log("Folder " + folderName + " (#" + (i + 1) + " of " + listFolders.Length + ")...");
+                if (folderName.Length != 3)
+                {
+                    Log("Folder " + folderName + " skipped.");
+                }
+                else
+                {
+                    string[] listFiles = Directory.GetFiles(folderPath);
+                    for (int f = 0; f < listFiles.Length; f++)
+                    {
+                        try
+                        {
+                            string filePath = listFiles[f];
+                            string fileName = filePath.Substring(filePath.LastIndexOf('\\') + 1);
+
+                            char[] fileNameArray = fileName.ToCharArray();
+                            int indexUnderline = fileName.IndexOf('_', fileName.IndexOf('_') + 1);
+                            fileNameArray[indexUnderline + 1] = folderName[0];
+                            fileNameArray[indexUnderline + 2] = folderName[1];
+
+                            indexUnderline = fileName.LastIndexOf('_', fileName.LastIndexOf('_') - 1);
+                            fileNameArray[indexUnderline + 1] = folderName[2];
+
+                            string newFileName = new string(fileNameArray);
+                            string newFilePath = filePath.Substring(0, filePath.LastIndexOf('\\') + 1) + newFileName;
+                            File.Move(filePath, newFilePath);
+                        }
+                        catch(Exception ex)
+                        {
+                            Log("Exception: " + ex.Message);
+                        }
+                    }
+                    Log(listFiles.Length + " files renamed successfully at folder " + folderName);
+                }
+                Log("---------------------------------------------------");
+            }
+            Log("Done :)");
+        }
+
+        void Log(string message)
+        {
+            lstLog.Items.Add(message);
+        }
+    }
+}
